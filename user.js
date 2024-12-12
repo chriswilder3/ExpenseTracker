b20c0607ca3b2324f7325f4ddf982cc37e2529ef
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-12 23:56:53 +0530

    correctly displayed all user details

--- user.js
+++ user.js
@@ -5,9 +5,28 @@ document.addEventListener('DOMContentLoaded',() =>{
 
     nameDispay.textContent = `Welcome ${currentProfileName},`;
     
-    const currentProfileInfo = localStorage.getItem(`${currentProfileName}`);
-    let profileInfoDisplay = document.querySelector('.profile-info')
-    profileInfoDisplay.textContent = `${currentProfileInfo}`
+    let currentProfileInfo = localStorage.getItem(`${currentProfileName}`);
+    
+    // let profileInfoDisplay = document.querySelector('.profile-info')
+    // profileInfoDisplay.textContent = `${currentProfileInfo}`
+
+    currentProfileInfo = JSON.parse(currentProfileInfo)
+
+    let profileSalaryDisplay = document.querySelector('.profile-salary');
+    profileSalaryDisplay.textContent = `Salary: ${currentProfileInfo['profileSalary']}`;
+
+    let profileCurrencyDisplay = document.querySelector('.profile-currency');
+    profileCurrencyDisplay.textContent = `Currency: ${currentProfileInfo['profileCurrency']}`;
+
+    let profileSavingsGoalDisplay = document.querySelector('.profile-savings-goal');
+    profileSavingsGoalDisplay.textContent = `Savings Goal: ${currentProfileInfo['profileSavingsGoal']}`;
+
+    let profileEmailDisplay = document.querySelector('.profile-email');
+    profileEmailDisplay.textContent = `Email: ${currentProfileInfo['profileEmail']}`;
+
+    let profileStartDateDisplay = document.querySelector('.profile-start-date');
+    profileStartDateDisplay.textContent = `Start Date: ${currentProfileInfo['profileStartDate']}`;
+
     
 
     
