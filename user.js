9f1a71ace752a938b9a24282820ac5b4f8263011
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-13 00:35:49 +0530

    calculate expenses and net income

--- user.js
+++ user.js
@@ -12,21 +12,43 @@ document.addEventListener('DOMContentLoaded',() =>{
 
     currentProfileInfo = JSON.parse(currentProfileInfo)
 
-    let profileSalaryDisplay = document.querySelector('.profile-salary');
-    profileSalaryDisplay.textContent = `Salary: ${currentProfileInfo['profileSalary']}`;
+    
+    const currency = currentProfileInfo['profileCurrency'] ;
 
-    let profileCurrencyDisplay = document.querySelector('.profile-currency');
-    profileCurrencyDisplay.textContent = `Currency: ${currentProfileInfo['profileCurrency']}`;
+    let profileSalaryDisplay = document.querySelector('.profile-salary');
+    let salary = currentProfileInfo['profileSalary'];
+
+    profileSalaryDisplay.textContent = `Total income : ${salary} ${currency}`;
 
     let profileSavingsGoalDisplay = document.querySelector('.profile-savings-goal');
-    profileSavingsGoalDisplay.textContent = `Savings Goal: ${currentProfileInfo['profileSavingsGoal']}`;
+    profileSavingsGoalDisplay.textContent = `Savings goal : ${currentProfileInfo['profileSavingsGoal']} ${currency}`;
 
     let profileEmailDisplay = document.querySelector('.profile-email');
-    profileEmailDisplay.textContent = `Email: ${currentProfileInfo['profileEmail']}`;
+    profileEmailDisplay.textContent = `Email : ${currentProfileInfo['profileEmail']}`;
 
     let profileStartDateDisplay = document.querySelector('.profile-start-date');
-    profileStartDateDisplay.textContent = `Start Date: ${currentProfileInfo['profileStartDate']}`;
+    profileStartDateDisplay.textContent = `Start Date : ${currentProfileInfo['profileStartDate']}`;
 
+    let profileExpensesDisplay = document.querySelector('.profile-expenses');
+    let totalExpenses = 0;
+
+    if('profileExpenses' in currentProfileInfo){
+        totalExpenses = currentProfileInfo['profileExpenes'];
+        profileExpensesDisplay.textContent = `Total expenses : ${totalExpenses} `;
+    }
+    else{
+        profileExpensesDisplay.innerHTML = 
+            `Total expenses : 0 <br/> <span class="text-base text-red-500"> Add more transactions! </span>`;
+    }
+
+    let netIncome = salary-totalExpenses;
+
+    let profileNetIncomeDisplay = document.querySelector('.profile-net-income');
+    profileNetIncomeDisplay.textContent = ` Your net income : ${netIncome}`;
+
+
+
+    
     
 
     
